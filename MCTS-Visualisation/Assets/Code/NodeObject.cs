1ce8e8c79f918717ec1149fbf4d711a295dee1b6
Michael Walsh <michaelwalsh223@hotmail.co.uk>
2017-10-08 18:41:09 +0100

    Added a 2D visualisation option

--- MCTS-Visualisation/Assets/Code/NodeObject.cs
+++ MCTS-Visualisation/Assets/Code/NodeObject.cs
@@ -17,24 +17,28 @@ public class NodeObject : Node {
     public int ChildPositionsSet { get; private set; }
 
     /// <summary>
+    /// The angle allowance this node has, used when visualising as a 2D disk
+    /// </summary>
+    public float ArcAngle { get; private set; }
+
+    /// <summary>
     /// Gives this <see cref=" NodeObject"/> a position in world space depending on its position in the game tree
     /// </summary>
 	public void SetPosition(VisualisationType visualisationType)
     {
-        NodeObject parentNode = (NodeObject)Parent;
+        NodeObject parentObject = (NodeObject)Parent;
+
+        //Play around with this value to change the structure of the tree depending on depth
+        float depthMul = 60;
 
         if (visualisationType == VisualisationType.Standard3D)
         {
             #region Standard 3D node placement
-            //Play around with this value to change the structure of the tree depending on depth
-            float depthMul = 60;
-
             //Root node, automatically starts at origin, does not require additional initialisation
             if (Depth == 0)
             {
                 return;
             }
-
 
             if (Depth == 1)             //If at depth 1, use the Fibonacci sphere algorithm to evenly distribute all depth 1 nodes in a sphere around the root node
             {
@@ -43,9 +47,9 @@ public class NodeObject : Node {
                 float offset = 2f / samples;
                 float increment = Mathf.PI * (3 - Mathf.Sqrt(5));
 
-                float y = ((parentNode.ChildPositionsSet * offset) - 1) + (offset / 2);
+                float y = ((parentObject.ChildPositionsSet * offset) - 1) + (offset / 2);
                 float r = Mathf.Sqrt(1 - Mathf.Pow(y, 2));
-                float phi = ((parentNode.ChildPositionsSet + 1) % samples) * increment;
+                float phi = ((parentObject.ChildPositionsSet + 1) % samples) * increment;
                 float x = Mathf.Cos(phi) * r;
                 float z = Mathf.Sin(phi) * r;
                 #endregion
@@ -53,11 +57,11 @@ public class NodeObject : Node {
             }
             else                        //If at any other depth, position the new node a set distance away from its parent node
             {
-                Position = parentNode.Position;
-                Position += parentNode.LocalPosition.normalized * depthMul * 2;
+                Position = parentObject.Position;
+                Position += parentObject.LocalPosition.normalized * depthMul * 2;
                 Vector3 rotationPoint = Position;
 
-                Vector3 normal = parentNode.LocalPosition;
+                Vector3 normal = parentObject.LocalPosition;
                 Vector3 tangent;
                 Vector3 t1 = Vector3.Cross(normal, Vector3.forward);
                 Vector3 t2 = Vector3.Cross(normal, Vector3.up);
@@ -74,7 +78,7 @@ public class NodeObject : Node {
                 if (Parent.Children.Count != 1)
                 {
                     Position += tangent.normalized * depthMul * 0.25f;
-                    Position = Position.RotateAround(rotationPoint, rotationPoint - parentNode.Position, (360 / Parent.Children.Capacity) * parentNode.ChildPositionsSet);
+                    Position = Position.RotateAround(rotationPoint, rotationPoint - parentObject.Position, (360 / Parent.Children.Capacity) * parentObject.ChildPositionsSet);
                 }
             }
             #endregion
@@ -82,7 +86,25 @@ public class NodeObject : Node {
         else if(visualisationType == VisualisationType.Disk2D)
         {
             #region Disk 2D node placement
+            //Root node, automatically starts at origin, does not require additional initialisation
+            if (Depth == 0)
+            {
+                ArcAngle = 360;
+                return;
+            }
 
+            if (Depth == 1)             //If at depth 1, place the new node on the edge of a circle around the origin
+            {
+                Position = new Vector3(60, 0, 0);
+                Position = Position.RotateAround(Vector3.zero, Vector3.up, parentObject.ChildPositionsSet * (parentObject.ArcAngle / parentObject.Children.Count));
+            }
+            else                        //If at any other depth, position the new node a set distance away from its parent node
+            {
+                Position = parentObject.Position;
+                Position += (parentObject.LocalPosition.normalized * depthMul);
+                Position = Position.RotateAround(parentObject.Position, Vector3.up, -(parentObject.ArcAngle / 2) + (parentObject.ChildPositionsSet * (parentObject.ArcAngle / parentObject.Children.Count)));
+            }
+            ArcAngle = parentObject.ArcAngle / parentObject.Children.Count;
             #endregion
         }
         else
@@ -90,7 +112,7 @@ public class NodeObject : Node {
             throw new System.Exception("Unknown visualisation type: " + visualisationType.ToString() + " encountered");
         }
 
-        parentNode.ChildPositionsSet++;
+        parentObject.ChildPositionsSet++;
     }    
 
     /// <summary>
