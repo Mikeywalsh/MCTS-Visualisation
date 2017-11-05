d3e7a31f9c941941fa31968f86ff79eed15953b4
Michael Walsh <michaelwalsh223@hotmail.co.uk>
2017-11-05 15:49:32 +0000

    Documented HashController

--- MCTS-Visualisation/Assets/Code/Experimental/HashController.cs
+++ MCTS-Visualisation/Assets/Code/Experimental/HashController.cs
@@ -3,22 +3,54 @@ using UnityEngine;
 using MCTS.Core;
 using MCTS.Core.Games;
 
+/// <summary>
+/// Main controller for the hashing visualisation <para/>
+/// In this visualisation, each board state is given its own position in world space and <see cref="LineRenderer"/> objects are used to create links between the nodes
+/// </summary>
 public class HashController : MonoBehaviour
 {
+    /// <summary>
+    /// A dictionary that maps unique positions in world space to their corresponding node gameobjects
+    /// </summary>
     public Dictionary<Vector3, GameObject> nodePositionMap = new Dictionary<Vector3, GameObject>();
 
+    /// <summary>
+    /// A dictionary that maps unique nodes to their corresponding node gameobjects
+    /// </summary>
     public Dictionary<Node, GameObject> nodeObjectMap = new Dictionary<Node, GameObject>();
 
+    /// <summary>
+    /// The MCTS used to provide node data
+    /// </summary>
     public TreeSearch<Node> mcts;
 
+    /// <summary>
+    /// The timestamp that the last step was performed <para/>
+    /// Used to determine when to do the next step
+    /// </summary>
     private float lastSpawn;
 
+    /// <summary>
+    /// Flag which controls whether the playing animation is active <para/>
+    /// When the playing animation is active, nodes are created periodically
+    /// </summary>
     bool playing = false;
 
+    /// <summary>
+    /// The delay in seconds between nodes being created when <see cref="playing"/> is active
+    /// </summary>
     private const float SPAWN_DELAY = 0.1f;
 
+    /// <summary>
+    /// The amount of spacing between each node
+    /// </summary>
     private const float NODE_SPACING = 7f;
 
+    /// <summary>
+    /// Called every frame <para/>
+    /// Used to control the creation of new nodes when <see cref="playing"/> is true <para/>
+    /// Also used to listen for user input
+    /// </summary>
     public void Update()
     {
         if (mcts == null)
@@ -46,6 +78,10 @@ public class HashController : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Called when the user enters the play/pause input <para/>
+    /// Toggles play/pause depending on which one is currently active
+    /// </summary>
     public void TogglePlayPause()
     {
         if (playing)
@@ -58,18 +94,31 @@ public class HashController : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Called when the play button is pressed <para/>
+    /// Starts the animation of ndoes being creates and sets the pause button to be active
+    /// </summary>
     public void PlayButtonPressed()
     {
         playing = true;
         HashUIController.PlayButtonPressed();
     }
 
+    /// <summary>
+    /// Called when the pause button is pressed <para/>
+    /// Pauses the animation of nodes beng created and sets the play button to active again
+    /// </summary>
     public void PauseButtonPressed()
     {
         playing = false;
         HashUIController.PauseButtonPressed();
     }
 
+    /// <summary>
+    /// Called when the start button is pressed <para/>
+    /// Initialises the <see cref="mcts"/> tree search object and instantiates the root node <para/>
+    /// Also creates as many starting nodes as the user specified
+    /// </summary>
     public void StartButtonPressed()
     {
         mcts = new TreeSearch<Node>(new TTTBoard());
@@ -93,6 +142,10 @@ public class HashController : MonoBehaviour
         HashUIController.SetNavigationPanelActive(true);
     }
 
+    /// <summary>
+    /// Called when the step button is pressed <para/>
+    /// Calls the <see cref="PerformStep(bool)"/> method as long as <see cref="playing"/> is false
+    /// </summary>
     public void StepButtonPressed()
     {
         if (playing)
@@ -103,6 +156,11 @@ public class HashController : MonoBehaviour
         PerformStep(false);
     }
 
+    /// <summary>
+    /// Performs a step of MCTS and creates a gameobject for the new <see cref="Node"/>'s board state if required <para/>
+    /// Also creates any relevant links between related nodes
+    /// </summary>
+    /// <param name="fromMenu">Flag used to indicate whether this step is being performed from the menu or not. Used to prevent the spawning animation if a node is not being added when the visualisation has already been started</param>
     public void PerformStep(bool fromMenu)
     {
         //Perform an iteration of MCTS
@@ -142,6 +200,11 @@ public class HashController : MonoBehaviour
         HashUIController.SetTotalNodeText(nodeObjectMap.Count);
     }
 
+    /// <summary>
+    /// Hashes a <see cref="Board"/> object to a unique position in world space
+    /// </summary>
+    /// <param name="board">The board to hash</param>
+    /// <returns>A <see cref="Vector3"/>, unique to the board state of hte board being hashed </returns>
     public Vector3 BoardToPosition(TTTBoard board)
     {
         float xPos = 0;
