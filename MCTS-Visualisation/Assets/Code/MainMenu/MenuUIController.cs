b28acc4bfb2fa0fdaf28f9e825ad8cace2677331
Michael Walsh <michaelwalsh223@hotmail.co.uk>
2017-11-20 16:13:09 +0000

    Created and documented a main menu UI controller

--- MCTS-Visualisation/Assets/Code/MainMenu/MenuUIController.cs
+++ MCTS-Visualisation/Assets/Code/MainMenu/MenuUIController.cs
@@ -1,14 +1,55 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class MenuUIController : MonoBehaviour {
+namespace MCTS.Visualisation.Menu
+{
+    /// <summary>
+    /// A singleton which controls the logic behind user interaction with UI elements in the main menu
+    /// </summary>
+    public class UIController : MonoBehaviour
+    {
+        /// <summary>
+        /// A singleton reference to the UIController
+        /// </summary>
+        private UIController controller;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        /// <summary>
+        /// Assign the singleton reference when the scene has started
+        /// </summary>
+        public void Start()
+        {
+            //If a singleton reference already exists, throw an exception explaining so
+            if(controller != null)
+            {
+                throw new System.Exception("UIController singleton is already assigned tO");
+            }
+
+            //Assign the singleton reference
+            controller = this;
+        }
+
+        /// <summary>
+        /// This method will be called when the tree visualisation button is pressed
+        /// </summary>
+        public static void TreeVisualisationButtonPressed()
+        {
+            SceneManager.LoadScene("TreeVisualisation");
+        }
+
+        /// <summary>
+        /// This method will be called when the hashing visualisation button is pressed
+        /// </summary>
+        public static void HashingVisualisationButtonPressed()
+        {
+            SceneManager.LoadScene("HashingVisualisation");
+        }
+
+        /// <summary>
+        /// This method will be called when the play connect four button is pressed
+        /// </summary>
+        public static void PlayConnectFourButtonPressed()
+        {
+            SceneManager.LoadScene("PlayConnect4");
+        }
+    }
 }
